1c9b65cbe4b495c68a01cc2688a8ac05d2d60140
GUS <tgtkhjrglgnzrg@gmail.com>
2026-05-05 12:28:58 +0530

    Updated UI for A1

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Hello React with Git 061 </h1>
+      <p>A1 Practical</p>
     </div>
   );
 }
